e87c9740429f681e196d23d876b1577ac25dd60f
mayconghizoni <maycon_ghizoni@hotmail.com>
2020-04-24 21:45:03 -0300

    Ajustes telas iniciais

---
+++ web/js/site.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+    $("#body").load("pages/webapp/home.html")   
+
+})
+
+home = function(){
+    $("#body").load("pages/webapp/home.html")  
+}
+
+cadastros = function(){
+    $("#body").load("pages/webapp/cadastrarExemplar.html") 
+}
+
+exemplares = function(){
+    $("#body").load("pages/webapp/exemplares.html")
+}
+
+emprestimos = function(){
+    $("#body").load("pages/webapp/emprestimos.html")
+}
+
+leitores = function(){
+    $("#body").load("pages/webapp/leitores.html")
+}
+
+relatorios = function(){
+    $("#body").load("pages/webapp/relatorios.html")
+}
+
+perfil = function(){
+    $("#body").load("pages/webapp/perfil.html")
+}
+
+// INICIO SUB PÁGINAS 
+
+cadastrarExemplar = function(){
+    $("#body").load("pages/webapp/cadastrarExemplar.html")  
+}
+
+cadastrarCategorias = function(){
+    $("#body").load("pages/webapp/cadastrarCategorias.html")  
+}
+
+cadastrarLeitores = function(){
+    $("#body").load("pages/webapp/cadastrarLeitores.html")  
+}

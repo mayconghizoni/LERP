afdf8a2faaf6f5cb899a1dd191ccccc99369201b
mayconghizoni <maycon_ghizoni@hotmail.com>
2020-05-04 20:11:56 -0300

    Ajustes front finalizados

--- out/artifacts/LERP_war_exploded/js/site.js
+++ out/artifacts/LERP_war_exploded/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("#body").load("pages/webapp/home.html")   
+    $("#body").load("pages/webapp/home.html")
 
 })
 
@@ -9,55 +9,66 @@ home = function(){
 }
 
 cadastros = function(){
-    $("#body").load("pages/webapp/cadastrarExemplar.html") 
+    $("#body").load("pages/webapp/cadastros/cadastrarExemplar.html")
 }
 
 exemplares = function(){
-    $("#body").load("pages/webapp/exemplaresDisponíveis.html")
+    $("#body").load("pages/webapp/exemplares/exemplaresDisponíveis.html")
 }
 
 emprestimos = function(){
-    $("#body").load("pages/webapp/emprestimosAtivos.html")
+    $("#body").load("pages/webapp/emprestimos/emprestimosAtivos.html")
 }
 
 leitores = function(){
-    $("#body").load("pages/webapp/leitores.html")
+    $("#body").load("pages/webapp/leitores/leitores.html")
 }
 
 relatorios = function(){
-    $("#body").load("pages/webapp/relatorios.html")
+    $("#body").load("pages/webapp/relatorios/relatorios.html")
 }
 
 perfil = function(){
-    $("#body").load("pages/webapp/perfil.html")
+    $("#body").load("pages/webapp/perfil/perfil.html")
 }
 
-// INICIO SUB PÁGINAS 
-
 cadastrarExemplar = function(){
-    $("#body").load("pages/webapp/cadastrarExemplar.html")  
+    $("#body").load("pages/webapp/cadastros/cadastrarExemplar.html")
 }
 
 cadastrarCategorias = function(){
-    $("#body").load("pages/webapp/cadastrarCategorias.html")  
+    $("#body").load("pages/webapp/cadastros/cadastrarCategorias.html")
 }
 
 cadastrarLeitores = function(){
-    $("#body").load("pages/webapp/cadastrarLeitores.html")  
+    $("#body").load("pages/webapp/cadastros/cadastrarLeitores.html")
 }
 
 exemplaresDisponiveis = function () {
-    $("#body").load("pages/webapp/exemplaresDisponíveis.html")
+    $("#body").load("pages/webapp/exemplares/exemplaresDisponíveis.html")
 }
 
 exemplaresManutencao = function () {
-    $("#body").load("pages/webapp/exemplaresManutencao.html")
+    $("#body").load("pages/webapp/exemplares/exemplaresManutencao.html")
 }
 
 emprestimos = function () {
-    $("#body").load("pages/webapp/emprestimosAtivos.html")
+    $("#body").load("pages/webapp/emprestimos/emprestimosAtivos.html")
 }
 
 novosEmprestimos = function () {
-    $("#body").load("pages/webapp/emprestimosNovos.html")
+    $("#body").load("pages/webapp/emprestimos/emprestimosNovos.html")
 }
+
+leitores = function () {
+    $("#body").load("pages/webapp/leitores/leitores.html")
+}
+
+leitoresDevedores = function () {
+    $("#body").load("pages/webapp/leitores/leitoresDevedores.html")
+}
+
+relatorios = function () {
+    $("#body").load("pages/webapp/relatorios/relatorios.html")
+}
+

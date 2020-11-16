7087009445fe8012e09f76da0ea4487f429667fe
mayconghizoni <maycon_ghizoni@hotmail.com>
2020-11-15 21:47:47 -0300

    Geração de relatórios

--- src/br/com/jdbc/JDBCRelatorioDAO.java
+++ src/br/com/jdbc/JDBCRelatorioDAO.java
@@ -1,8 +1,10 @@
 package br.com.jdbc;
 
 import br.com.jdbcinterface.RelatorioDAO;
+import br.com.modelo.Grafico;
 import br.com.modelo.Relatorio;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -53,6 +55,36 @@ public class JDBCRelatorioDAO implements RelatorioDAO {
         }
 
         return listaRelatorio;
+    }
+
+    public Grafico buscarGrafico (String dataInicio, String dataFim){
+
+        String comando = "select count(he.idCategoria) as quantidade, c.descricao from historico_emprestimos he " +
+                         "inner join categoria c on c.idCategoria = he.idCategoria " +
+                         "where date(he.dataSaida) between '" + dataInicio + "' and '" + dataFim + "' " +
+                         "group by he.idCategoria;";
+
+        List<Integer> quantidades = new ArrayList<Integer>();
+        List<String> categorias = new ArrayList<String>();
+        Grafico grafico = new Grafico();
+        try{
+
+            Statement stmt = conexao.createStatement();
+            ResultSet rs = stmt.executeQuery(comando);
+
+            while (rs.next()){
+
+                quantidades.add(rs.getInt("quantidade"));
+                categorias.add(rs.getString("descricao"));
+
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        grafico.setCategoria(categorias);
+        grafico.setQuantidade(quantidades);
+        return grafico;
     }
 
 }

a23377b14d93c8a7af06ac21445540c1f997080e
maycon <maycon_ghizoni@hotmail.com>
2020-12-09 20:36:24 -0300

    Ajustes gerais

--- src/br/com/bd/Conexao.java
+++ src/br/com/bd/Conexao.java
@@ -9,8 +9,8 @@ public class Conexao {
     public Connection abrirConexao(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");//Seleciona driver JDBC
-            conexao = java.sql.DriverManager.getConnection("jdbc:mysql://localhost/bd_LERP?" +
-                    "user=maycon&password=root&useTimezone=true&serverTimezone=UTC"); //Passas os parametros da conexão atual
+            conexao = java.sql.DriverManager.getConnection("jdbc:mysql://localhost/bd_lerp?" +
+                    "user=root&password=as&useTimezone=true&serverTimezone=UTC"); //Passas os parametros da conexão atual
         }catch (Exception e){
             e.printStackTrace();
         }
